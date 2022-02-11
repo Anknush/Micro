875c75c43244e0fb2fe96687c0d5161153ac97e7
Ankush Kaundal <ankush.kaundal@ericsson.com>
2022-02-11 15:58:26 +0530

    Complete user Controller and add UserNotFoundException

--- AnkushDemo/src/main/java/com/example/AnkushDemo/userData/UserController.java
+++ AnkushDemo/src/main/java/com/example/AnkushDemo/userData/UserController.java
@@ -1,5 +1,25 @@
 package com.example.AnkushDemo.userData;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+@RestController
+public class UserController {
+	@Autowired
+	UserDao userdao;
+	@GetMapping("/users")
+	public List<UserModel> allUsers() {
+		return userdao.findAll();
+	}
+	@GetMapping("/users/{id}")
+	public UserModel findById(@PathVariable int id) {
+		UserModel user=userdao.findOne(id);
+		if(user.getId()==0) {
+			throw new UserNotFoundException("User not found Id: "+id);
+		}
+		return user;
+	}
 }
--- AnkushDemo/src/main/java/com/example/AnkushDemo/userData/UserDao.java
+++ AnkushDemo/src/main/java/com/example/AnkushDemo/userData/UserDao.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDao {
 	private static List<UserModel> user=new ArrayList<>();
+	private int userCount=3;
 	static {
 		user.add(new UserModel(1,"Ankush",new Date()));
 		user.add(new UserModel(2,"Aditya",new Date()));
@@ -12,5 +13,31 @@ public class UserDao {
 	}
 	public List<UserModel> findAll(){
 		return user;
+	}
+	public UserModel findOne(int id) {
+		for(UserModel users:user) {
+			if(users.getId()==id) {
+				return users;
+			}
+		}
+		return null;
+	}
+	public UserModel saveUser(UserModel users) {
+		if(users.getId()==0) {
+			users.setId(+userCount);
+		}
+		user.add(users);
+		return users;
+	}
+	public UserModel deleteUser(int id) {
+		Iterator<UserModel> itr=user.iterator();
+		while(itr.hasNext()) {
+			UserModel users=itr.next();
+			if(users.getId()==id) {
+				user.remove(id);
+				return users;
+			}
+		}
+		return null;
 	}
 }
