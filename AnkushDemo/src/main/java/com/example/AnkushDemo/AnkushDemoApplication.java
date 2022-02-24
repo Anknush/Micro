f6d32a44b5abebd97552befdccc76e3f6e94c4af
Ankush Kaundal <ankush.kaundal@ericsson.com>
2022-02-24 17:39:09 +0530

    Adding swagger

--- AnkushDemo/src/main/java/com/example/AnkushDemo/AnkushDemoApplication.java
+++ AnkushDemo/src/main/java/com/example/AnkushDemo/AnkushDemoApplication.java
@@ -1,10 +1,14 @@
 package com.example.AnkushDemo;
 
+
+//import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
@@ -14,5 +18,11 @@ public class AnkushDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AnkushDemoApplication.class, args);
 	}
-
+//	@Bean
+//	public LocaleResolver localeResolver() {
+//		SessionLocaleResolver localeResolver=new SessionLocaleResolver();
+//		localeResolver.setDefaultLocale(Locale.US);
+//		return localeResolver;
+//	}
+	
 }
